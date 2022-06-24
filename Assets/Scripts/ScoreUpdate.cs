1ca6dfc81411ae8c00a2d0834d399ad7f43ad156
Abdullah Ibrahim <abdullah.ibrahim23@hotmail.com>
2022-06-23 23:03:24 -0400

    Bug fixes maybe??
    -Added score gain animations
    -Fixed hitbox issues*
    -Fixed navmesh agent issues*

--- Assets/Scripts/ScoreUpdate.cs
+++ Assets/Scripts/ScoreUpdate.cs
@@ -7,15 +7,22 @@ public class ScoreUpdate : MonoBehaviour
 {
     [SerializeField] public int scoreTotal;
     private TextMeshProUGUI scoreText;
+    private Animator anim;
+    private TextMeshProUGUI scoreGainText;
 
     private void Start()
     {
         scoreText = gameObject.GetComponent<TextMeshProUGUI>();
+        anim = GameObject.Find("ScoreGain").GetComponent<Animator>();
         scoreText.text = ("" + 0);
     }
 
     public void updateScore()
     {
+        int scoreGainAnim = Random.Range(0, 3);
+        if(scoreGainAnim == 0) { anim.Play("ScoreGain", 0); }
+        if(scoreGainAnim == 1) { anim.Play("ScoreGain_2", 0); }
+        if(scoreGainAnim == 2) { anim.Play("ScoreGain_3", 0); }
         scoreText.text = ("" + scoreTotal);
     }
 }
