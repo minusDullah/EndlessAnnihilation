c609dadc4c3edf74e21f3af0442f0a6be4896e57
Abdullah Ibrahim <abdullah.ibrahim23@hotmail.com>
2022-06-28 00:33:38 -0400

    Added other enemies
    -Added 9 other enemy models

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+
+    [SerializeField] public AudioMixer _MasterMixer;
+    
+    private float startingVolume;
+    public Slider[] slider;
+
+    private void Start()
+    {
+        UpdateMasterSlider(slider[0]);
+        UpdateMusicSlider(slider[1]);
+        UpdatePlayerAudioSlider(slider[2]);
+        UpdateEnemySlider(slider[3]);
+    }
+
+    public void UpdateMasterSlider(Slider volume)
+    {
+        _MasterMixer.GetFloat("Master", out startingVolume);
+        _MasterMixer.SetFloat("Master", startingVolume);
+        volume.value = startingVolume;
+    }
+
+    public void UpdateMusicSlider(Slider volume)
+    {
+        _MasterMixer.GetFloat("Music", out startingVolume);
+        _MasterMixer.SetFloat("Music", startingVolume);
+        volume.value = startingVolume;
+    }
+
+    public void UpdatePlayerAudioSlider(Slider volume)
+    {
+        _MasterMixer.GetFloat("PlayerAudio", out startingVolume);
+        _MasterMixer.SetFloat("PlayerAudio", startingVolume);
+        volume.value = startingVolume;
+    }
+
+    public void UpdateEnemySlider(Slider volume)
+    {
+        _MasterMixer.GetFloat("Enemy", out startingVolume);
+        _MasterMixer.SetFloat("Enemy", startingVolume);
+        volume.value = startingVolume;
+    }
+
+
+    public void SetMasterVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("Master", volume.value);
+    }
+
+    public void SetMusicVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("Music", volume.value);
+    }
+
+    public void SetPlayerVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("PlayerAudio", volume.value);
+    }
+    
+    public void SetEnemyVolume(Slider volume)
+    {
+        _MasterMixer.SetFloat("Enemy", volume.value);
+    }
+
+}
